1724071355071e1523cf29c9db92257a0a17aba7
LUCA OLANDIM MELLO <luca_mello@estudante.sesisenai.org.br>
2024-08-06 11:48:26 -0300

    Navegação de Componentes de Página

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,37 @@
 import React, { useState } from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import Destino from './Destino';
-import FormDestino from './FormDestino';
+import Header from './componensts/main/Header';
+import Footer from './componensts/main/Footer';
+import Destinos from './componensts/pages/Destinos';
+import Contato from './componensts/pages/Contato';
+import Home from './componensts/pages/Home';
+import './App.css';
 
 function App() {
-  const [destinos, setDestinos] = useState([
-    { nome: 'Paris', descricao: 'A cidade do amor.' },
-    { nome: 'Nova York', descricao: 'A cidade que nunca dorme.' },
-    { nome: 'Tóquio', descricao: 'A capital do Japão.' }
-  ]);
+  const [paginaAtual, setPaginaAtual] = useState('Home');
 
-  const adicionarDestino = (novoDestino) => {
-    setDestinos([...destinos, novoDestino]);
+  const renderPagina = () => {
+	switch (paginaAtual) {
+	  case 'Home':
+		return <Home/>;
+	  case 'Destinos':
+		return <Destinos />;
+	  case 'Contato':
+		return <Contato/>;
+	  default:
+		return <Home />;
+	}
   };
+
 
   return (
     <div className="App">
       <Header />
-      <FormDestino adicionarDestino={adicionarDestino} />
-      {destinos.map((destino, index) => (
-        <Destino key={index} nome={destino.nome} descricao={destino.descricao} />
-      ))}
+      <nav>
+        <button onClick={() => setPaginaAtual('Home')}>Home</button>
+        <button onClick={() => setPaginaAtual('Destinos')}>Destinos</button>
+        <button onClick={() => setPaginaAtual('Contato')}>Contato</button>
+      </nav>
+      {renderPagina()}
       <Footer />
     </div>
   );
